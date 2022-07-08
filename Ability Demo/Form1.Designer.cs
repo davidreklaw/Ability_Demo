466ea5f96824beb5718a3fd3c939ee4da23f0e6c
David Walker <thewalker.david@gmail.com>
2022-07-08 10:31:44 -0400

    File uploading

---
+++ Ability Demo/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Ability_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uc1Button = new System.Windows.Forms.Button();
+            this.uc2Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uc1Button
+            // 
+            this.uc1Button.Location = new System.Drawing.Point(69, 325);
+            this.uc1Button.Name = "uc1Button";
+            this.uc1Button.Size = new System.Drawing.Size(136, 23);
+            this.uc1Button.TabIndex = 0;
+            this.uc1Button.Text = "Add UC1";
+            this.uc1Button.UseVisualStyleBackColor = true;
+            this.uc1Button.Click += new System.EventHandler(this.uc1Button_Click);
+            // 
+            // uc2Button
+            // 
+            this.uc2Button.Location = new System.Drawing.Point(263, 325);
+            this.uc2Button.Name = "uc2Button";
+            this.uc2Button.Size = new System.Drawing.Size(128, 23);
+            this.uc2Button.TabIndex = 1;
+            this.uc2Button.Text = "Add UC2";
+            this.uc2Button.UseVisualStyleBackColor = true;
+            this.uc2Button.Click += new System.EventHandler(this.uc2Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 380);
+            this.Controls.Add(this.uc2Button);
+            this.Controls.Add(this.uc1Button);
+            this.Name = "Form1";
+            this.Text = "Ability Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uc1Button;
+        private System.Windows.Forms.Button uc2Button;
+    }
+}
+
---
+++ Ability Demo/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ability_Demo
+{
+    public partial class Form1 : Form
+    {
+        private bool labelExists = false;
+        private bool textExists = false;
+        private Label label;
+        private TextBox text;
+        private Size formSize;
+        private int controlWidth;
+        private int controlHeight;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.formSize = this.Size;
+            double width = this.formSize.Width * .75;
+            double height = this.formSize.Height * .20;
+            this.controlWidth = (int)Math.Ceiling(width);
+            this.controlHeight = (int)Math.Ceiling(height);
+        }
+
+        private void uc1Button_Click(object sender, EventArgs e)
+        {
+            if (this.labelExists)
+            {
+                this.labelExists = false;
+                this.Controls.Remove(this.label);
+                this.uc1Button.Text = "Add UC1";
+            }
+            else
+            {
+                this.label = new Label();
+                this.label.Text = "User Control 1";
+                this.label.Height = this.controlHeight;
+                this.label.Width = this.controlWidth;
+                this.label.Location = new Point(30,30);
+                this.Controls.Add(this.label);
+                this.labelExists = true;
+                this.uc1Button.Text = "Remove UC1";
+            }
+        }
+
+        private void uc2Button_Click(object sender, EventArgs e)
+        {
+            if (this.textExists)
+            {
+                this.Controls.Remove(this.text);
+                this.textExists = false;
+                this.uc2Button.Text = "Add UC2";
+            }
+            else
+            {
+                this.text = new TextBox();
+                this.text.Text = "User Control 2";
+                this.text.Width = this.controlWidth;
+                this.text.Height = this.controlHeight;
+                this.text.Location = new Point(30, 70);
+                this.Controls.Add(this.text);
+                this.textExists = true;
+                this.uc2Button.Text = "Remove UC2";
+            }
+        }
+    }
+}
